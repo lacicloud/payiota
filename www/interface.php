7b98fbd55f239f1a37bc34cafe362bcb1640d4fb
Isaiah N <isaiah@stayawhile.com>
2017-11-14 22:35:14 -0500

    Update PayIOTA Interface
    Update PayIOTA Interface

--- www/interface.php
+++ www/interface.php
@@ -34,56 +34,76 @@ if (isset($_POST["ipn_url_new"])) {
 <html>
 <head>
 	<title>PayIOTA - Interface</title>
-		<?php include('header.php'); ?>
-		<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-		<style>
-			div, p, h2 {
-				text-align: center;
-			}
-		</style>
-
+	<?php include('header.php'); ?>
+	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	<meta property="og:image" content="https://payiota.me/resources/payiota_icon.png"/>
 </head>
 <body>
 
-<?php 
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+  <div class="container">
+	<a class="navbar-brand" href="/"><img src="resources/payiota_logo3.png" height="40" alt=""></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+	  <span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+	  <ul class="navbar-nav ml-auto">
+		<li class="nav-item">
+		  <strong class="nav-link account_name"><?php echo $data["email"];?></strong>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="mailto:support@payiota.me">Support</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="https://github.com/lacicloud/payiota/wiki/PayIOTA-API-Documentation">Documents</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="?logout=true">Logout</a>
+		</li>
+	  </ul>
+	</div>
+  </div>
+</nav>
+	
+<section class="content-section-b api_details">
+	<div class="warning_list">
+		<div class="success"></div>
+		<div class="warning"></div>
+		<div class="error"></div>
+		<div class="info"></div>
+	</div>
 
-echo "<h2 style='text-align: center;'>Welcome ".$data["email"]."!</h2><br><br>";
+	<div class="api_list">
+		<table>
+		  <tr><td>ID:</td><td class="api_data"><?php echo $id;?></td></tr>
+		  <tr><td>IOTA seed:</td><td class="api_data"><?php echo $data["seed"];?></td></tr>
+		  <tr><td>API key:</td><td class="api_data"><?php echo $data["api_key"];?></td></tr>
+		  <tr><td>Verification:</td><td class="api_data"><?php echo $data["verification"];?></td></tr>
+		  <tr><td>Your IPN URL:</td><td class="api_data">
+		  <?php if($data["ipn_url"]) { ?>
+			<a href="<?php echo $data["ipn_url"];?>"><?php echo $data["ipn_url"];?></a>
+		  <?php } else { ?>
+			<a class="no_ipn" href="#">No IPN URL Added</a>
+		  <?php } ?>
+		  </td></tr>
+		  <tr><td>Number of addresses:</td><td class="api_data"><?php echo $count;?></td></tr>
+		  <tr><td>Total balance:</td><td class="api_data"><?php echo $total_balance;?> MI</td></tr>
+		</table>
 
-?>
+		<a href="?refresh=true">Refresh Balance</a>
 
-<a href="?logout=true" id="logout">Logout</a>
-
-<div>
-<div class="success"></div>
-<div class="warning"></div>
-<div class="error"></div>
-<div class="info"></div>
-</div>
-
-<div>
-<?php 
-echo "ID: ".$id."<br>";
-echo "IOTA seed: ".$data["seed"]."<br>";
-echo "API key: ".$data["api_key"]."<br>";
-echo "Verification: ".$data["verification"]."<br>";
-echo "Your IPN URL: ".$data["ipn_url"]."<br>";
-echo "Number of addresses: ".$count."<br>";
-echo "Total balance: ".$total_balance."<br>";
-?> <a href="?refresh=true">Refresh Balance</a><br><br> <?php 
-?>
-
-<p>Update IPN URL:</p>
-<form action="#" method="POST" onsubmit="return ValidateURL(this);">
-	  <input required type="text" name="ipn_url_new" placeholder="new ipn url"/>
-	  <br><br>
-      <input type="submit" value="Update IPN"></li>  
-</form>
-<br>
-
-</div>
+		<h4>Update IPN URL:</h4>
+		<form action="#" method="POST" onsubmit="return ValidateURL(this);">
+			  <input required class="ipn_url_new" type="text" name="ipn_url_new" placeholder="New IPN URL Address"/>
+			  <input type="submit" value="Update IPN"></li>  
+		</form>
+	</div>
+</section>
+<!-- /.content-section-b -->
 
 
-
+<section class="api_payments">
 <h2>Your API calls</h2><br>
 <p>Scroll right on mobile!</p>
 
@@ -107,39 +127,33 @@ echo "Total balance: ".$total_balance."<br>";
 		<tr>
 		  <th scope='row'>".$count."</th>
 		  <td><a href='https://iotasear.ch/hash/".$value[0]["address"]."' target='_blank'>".$value[0]["address"]."</a></td>
-		  <td>$".$value[0]["price"]."</td>
-		  <td>".$value[0]["price_iota"]."</td>
-		  <td>".$value[0]["custom"]."</td>
-		  <td>".$api->getAddressBalance($value[0]["address"])."</td>
-		  <td>".$value[0]["done"]."</td>
+		  <td><span>Price (USD)</span>		$".$value[0]["price"]."</td>
+		  <td><span>Price in IOTA</span>	".$value[0]["price_iota"]."</td>
+		  <td><span>Custom Variable</span>	".$value[0]["custom"]."</td>
+		  <td><span>Balance</span>			".$api->getAddressBalance($value[0]["address"])."</td>
+		  <td><span>Status</span>			".$value[0]["done"]."</td>
 		</tr>";
 		$count++;
 	}
 	?>
   </tbody>
 </table>
+</section>
 
 <script src="js/main.js"></script>
-
 <script>
+	var success = document.getElementsByClassName("success")[0];
+	var error = document.getElementsByClassName("error")[0];
+	var info = document.getElementsByClassName("info")[0];
+	var warning = document.getElementsByClassName("warning")[0];
 
-var success = document.getElementsByClassName("success")[0];
-var error = document.getElementsByClassName("error")[0];
-var info = document.getElementsByClassName("info")[0];
-var warning = document.getElementsByClassName("warning")[0];
-
-<?php 
-
-if (isset($result)) {
-
-    echo "".$api->matchCodeToType($result).".innerHTML='".$api->matchCodeToMessage($result)."';";
-    echo "\n";
-    echo "".$api->matchCodeToType($result).".style.display = 'block';";
-}
-
-
-?>
-
+	<?php 
+	if (isset($result)) {
+		echo "".$api->matchCodeToType($result).".innerHTML='".$api->matchCodeToMessage($result)."';";
+		echo "\n";
+		echo "".$api->matchCodeToType($result).".style.display = 'block';";
+	}
+	?>
 </script>
 
 </body>
